0792522d3ed22a47dc9161250f822a4a8b6c362b
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-11-19 15:29:48 +0200

    Build a source for quotes, and insert a quote into the website randomly

--- src/quote-machine.jsx
+++ src/quote-machine.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
+import QUOTE_DATA from './quote-data';
 
 class QuoteMachine extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Quote: ''
+      collections: QUOTE_DATA,
+      QuoteText: "",
+      QuoteAuthor: ""
     };
+  }
+
+  componentDidMount() {
+    const rendum = this.state.collections[Math.floor(Math.random()*10)]
+    this.setState({
+      collections: QUOTE_DATA,
+      QuoteText: rendum.text,
+      QuoteAuthor: rendum.author
+    })
   }
 
   render() {
     return (
       <div>
         <div id="quote-box">
-          <h2 id="text">הצלחה היא היכולת לעבור מכישלון לכישלון מבלי לאבד את ההתלהבות</h2>
-          <h3 id="author">וינסטון צ'רצ'יל</h3>
-          <button id="new-quote">ציטוט חדש</button>
+          <h2 id="text">{this.state.QuoteText}</h2>
+          <h3 id="author">{this.state.QuoteAuthor}</h3>
+          <button id="new-quote">
+            !ציטוט חדש
+          </button>
           <a id="tweet-quote" href='"twitter.com/intent/tweet"'>
           twitter
           </a>
