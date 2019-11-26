9e7476e571bf23f9357ea67e8b906768747a958a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-11-26 14:39:55 +0200

    Adds the mapDispatchToProps function and finish adding a redox to the app

--- src/quote-machine.jsx
+++ src/quote-machine.jsx
@@ -3,53 +3,46 @@ import { connect } from 'react-redux'
 import './quote-machine.css';
 import QUOTE_DATA from './quote-data';
 import  './image/twitter.png';
+import { setCurrentQuote } from './redux/quote.actions'
 
 
 class QuoteMachine extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      collections: QUOTE_DATA,
-
-    };
     this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    const rendum = this.state.collections[Math.floor(Math.random()*this.state.collections.length)]
-    this.setState({
-      quote: {
+    const rendum = QUOTE_DATA[Math.floor(Math.random()*QUOTE_DATA.length)]
+      this.props.setCurrentQuote({
         text: rendum.text,
         author: rendum.author
-      }
-    })
+      })
   }
 
   handleClick() {
-    const rendum = this.state.collections[Math.floor(Math.random()*this.state.collections.length)]
-    this.setState({
-      quote: {
-        text: rendum.text,
-        author: rendum.author
-      }
+    const rendum = QUOTE_DATA[Math.floor(Math.random()*QUOTE_DATA.length)]
+    this.props.setCurrentQuote({
+      text: rendum.text,
+      author: rendum.author
     })
   }
 
   render() {
     const { quote } = this.props;
     return (
-        <div id="quote-box" >
-          <h2 id="text">{quote.text}</h2>
-          <h3 id="author">{quote.author}</h3>
-          <div id="buttonAndA" >
-            <a id="tweet-quote" className="twitter-share-button" href={`https://twitter.com/intent/tweet?text=${quote.text}--${quote.author}`} >
-              <img src="twitter.png" alt="twitter" width='10px' height='10px' />       
-            </a>
-            <button id="new-quote" onClick={this.handleClick} >
-              ציטוט חדש
-            </button>
-          </div>
+      <div id="quote-box" >
+        <h2 id="text">{quote.text}</h2>
+        <h3 id="author">{quote.author}</h3>
+        <div id="buttonAndA" >
+          <a id="tweet-quote" className="twitter-share-button" href={`https://twitter.com/intent/tweet?text=${quote.text}--${quote.author}`} >
+            <img src="twitter.png" alt="twitter" width='10px' height='10px' />       
+          </a>
+          <button id="new-quote" onClick={this.handleClick} >
+            ציטוט חדש
+          </button>
         </div>
+      </div>
     )
   }
 }
@@ -58,4 +51,8 @@ const mapStateToProps = state => ({
       quote: state.quote
 });
 
-export default connect(mapStateToProps)(QuoteMachine);
+const mapDispatchToProps = dispatch => ({
+  setCurrentQuote: quote => dispatch(setCurrentQuote(quote))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuoteMachine);
