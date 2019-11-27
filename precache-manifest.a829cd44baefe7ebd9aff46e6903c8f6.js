10f3366d37f01742181ac15e03d65e8fb45796dd
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-11-27 10:15:43 +0200

    Updates

---
+++ precache-manifest.a829cd44baefe7ebd9aff46e6903c8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba96420551e7cefebdefa3174b93c36a",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "3b73b6809e81b053b65b",
+    "url": "/Random-Quote-Machine/static/css/main.a4db5512.chunk.css"
+  },
+  {
+    "revision": "4e9d8afca378c97a1814",
+    "url": "/Random-Quote-Machine/static/js/2.c278ef7f.chunk.js"
+  },
+  {
+    "revision": "3b73b6809e81b053b65b",
+    "url": "/Random-Quote-Machine/static/js/main.0b01b6d1.chunk.js"
+  },
+  {
+    "revision": "3c41e596792c7a8adff4",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.a81697ae.js"
+  },
+  {
+    "revision": "6383b1040cdec8c8b40ee0e212fd57d1",
+    "url": "/Random-Quote-Machine/static/media/aaa.6383b104.jpg"
+  }
+]);
